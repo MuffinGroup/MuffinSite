9b83f0f2b749965c0726959551a2e491df1849d3
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-05-06 14:29:05 +0200

    added not at all suspicious link :)

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ var button = document.getElementById("button1");
 
 button.addEventListener("click", function() {
   changeTitle(Math.floor(Math.random() * (100 - 1 + 1)) + 1)
+  openSite("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 });
 
 function changeTitle(title) {
   document.title = title;
+}
+
+function openSite(link) {
+  window.open(link,  "_blank")
 }
