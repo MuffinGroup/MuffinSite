72c776d320b29d69fc6573c72ad7f42a38a743ea
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-05-06 11:16:50 +0200

    switched back to js :)

---
+++ script.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("button1");
+
+button.addEventListener("click", function() {
+  // Define the action to perform when the button is clicked
+  alert("Button clicked!");
+});
