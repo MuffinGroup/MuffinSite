3e0ad2747ad2a3252bfcf43c595c3b7871a98528
thepigcat76 <93595333+taraxsmallboy@users.noreply.github.com>
2023-05-11 19:31:22 +0200

    more updates

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var button = document.getElementById("button1");
+var iconButton = document.getElementsByClassName("githubButton")
+var defaultTitle = "Muffin Group"
 
 button.addEventListener("click", function() {
   changeTitle(Math.floor(Math.random() * (100 - 1 + 1)) + 1)
   openSite("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 });
+
+iconButton.addEventListener("mouseover", function() {
+  this.style.cursor = ""
+})
 
 function changeTitle(title) {
   document.title = title;
@@ -12,16 +18,3 @@ function changeTitle(title) {
 function openSite(link) {
   window.open(link,  "_blank")
 }
-
-document.addEventListener('mousemove', function(event) {
-  var followingImage = document.getElementById('following-image');
-  var container = document.querySelector('.container');
-
-  var containerRect = container.getBoundingClientRect();
-
-  var mouseX = event.clientX - containerRect.left;
-  var mouseY = event.clientY - containerRect.top;
-
-  followingImage.style.left = mouseX + 'px';
-  followingImage.style.top = mouseY + 'px';
-});
