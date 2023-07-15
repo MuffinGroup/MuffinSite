dca0c3cbf9a311e55d68e0891520bd0b549efd86
benpospo@gmail.com <93595333+taraxsmallboy@users.noreply.github.com>
2023-07-15 20:48:40 +0200

    added pages & footer

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 var ytButton = document.getElementById("youtubeButton");
 var dcButton = document.getElementById("discordButton");
 var ghButton = document.getElementById("githubButton");
-var defaultTitle = "Muffin Group"
+var defaultTitle = "Muffin Group";
 
 var ytbng_git = document.getElementById("ytbng_git");
 var muffin_site_git = document.getElementById("ms_git");
 var factorization_git = document.getElementById("fac_git");
 
-var contactButton = document.getElementById("link_button")
+var contactButton = document.getElementById("link_button");
 
-ghButton.addEventListener("click", function() {
-  openSite("https://github.com/MuffinGroup")
+ghButton.addEventListener("click", function () {
+  openSite("https://github.com/MuffinGroup");
 });
 
-ytButton.addEventListener("click", function() {
-  openSite("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+ytButton.addEventListener("click", function () {
+  openSite("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 });
 
-dcButton.addEventListener("click", function() {
-  openSite("https://discord.gg/XGvWxtgaaB")
+dcButton.addEventListener("click", function () {
+  openSite("https://discord.gg/XGvWxtgaaB");
 });
 
-contactButton.addEventListener("click", function() {
-  scrollToSection(document.getElementById("contact"))
-  openSite("https://github.com/MuffinGroup/yet-to-be-named-game")
-})
+contactButton.addEventListener("click", function () {
+  scrollToSection(document.getElementById("contact"));
+  openSite("https://github.com/MuffinGroup/yet-to-be-named-game");
+});
 
-ytbng_git.addEventListener("click", function() {
-  openSite("https://github.com/MuffinGroup/yet-to-be-named-game")
-})
+ytbng_git.addEventListener("click", function () {
+  openSite("https://github.com/MuffinGroup/yet-to-be-named-game");
+});
 
-const button = document.getElementById('ytbng_git');
+const button = document.getElementById("ytbng_git");
 
-button.addEventListener('click', function() {
-  console.log('Button clicked!');
-  openSite("https://github.com/MuffinGroup/yet-to-be-named-game")
+button.addEventListener("click", function () {
+  console.log("Button clicked!");
+  openSite("https://github.com/MuffinGroup/yet-to-be-named-game");
 });
 
 function changeTitle(title) {
@@ -42,10 +42,10 @@ function changeTitle(title) {
 }
 
 function openSite(link) {
-  window.open(link,  "_blank")
+  window.open(link, "_blank");
 }
 
 function scrollToSection() {
-    var section = document.getElementById("contact")
-    section.scrollIntoView({ behavior: "smooth" })
+  var section = document.getElementById("contact");
+  section.scrollIntoView({ behavior: "smooth" });
 }
